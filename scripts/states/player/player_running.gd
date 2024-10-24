644005f60da1dda2a521e85464a0a20329699ef6
Steven <steven@opensuse-laptop.hsd1.ca.comcast.net>
2024-10-23 17:55:27 -0700

    Added directional control over the player sprite. Attempted to add a bounce when hitting a way, but raycasts suck.

--- scripts/states/player/player_running.gd
+++ scripts/states/player/player_running.gd
@@ -8,6 +8,14 @@ func enter(previous_state_path: String, entity_data := {}):
 	
 func process_physics(delta: float) -> void:
 	var input_direction_x := Input.get_axis("ui_left", "ui_right")
+	if input_direction_x != 0:
+		player.direction = input_direction_x
+		if player.sprite.scale.x < 0 and player.direction == 1:
+			player.sprite.scale.x = player.sprite.scale.x * -1
+			print("facing left")
+		elif player.sprite.scale.x > 0 and player.direction == -1:
+			player.sprite.scale.x = player.sprite.scale.x * -1
+			print("facing right")
 	player.velocity.x = player.speed * input_direction_x
 	player.velocity.y += player.gravity * delta
 	player.move_and_slide()
